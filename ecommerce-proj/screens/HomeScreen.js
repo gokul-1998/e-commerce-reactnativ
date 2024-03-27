f7552512218d7a0e712cbe7b0fad328be7c35229
Gokul <82328858+gokul-1998@users.noreply.github.com>
2024-03-27 16:49:58 +0530

    BUG

--- ecommerce-proj/screens/HomeScreen.js
+++ ecommerce-proj/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { Platform, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Image, Platform, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { AntDesign } from '@expo/vector-icons';
@@ -8,6 +8,43 @@ import { Ionicons } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 
 const HomeScreen = () => {
+    const list = [
+        {
+          id: "0",
+          image: "https://m.media-amazon.com/images/I/41EcYoIZhIL._AC_SY400_.jpg",
+          name: "Home",
+        },
+        {
+          id: "1",
+          image:
+            "https://m.media-amazon.com/images/G/31/img20/Events/Jup21dealsgrid/blockbuster.jpg",
+          name: "Deals",
+        },
+        {
+          id: "3",
+          image:
+            "https://images-eu.ssl-images-amazon.com/images/I/31dXEvtxidL._AC_SX368_.jpg",
+          name: "Electronics",
+        },
+        {
+          id: "4",
+          image:
+            "https://m.media-amazon.com/images/G/31/img20/Events/Jup21dealsgrid/All_Icons_Template_1_icons_01.jpg",
+          name: "Mobiles",
+        },
+        {
+          id: "5",
+          image:
+            "https://m.media-amazon.com/images/G/31/img20/Events/Jup21dealsgrid/music.jpg",
+          name: "Music",
+        },
+        {
+          id: "6",
+          image: "https://m.media-amazon.com/images/I/51dZ19miAbL._AC_SY350_.jpg",
+          name: "Fashion",
+        },
+      ];
+
     return (
         <SafeAreaView style={{ paddingTop: Platform.OS === "android" ? 40 : 0, flex: 1, backgroundColor: "white" }}>
             <ScrollView>
@@ -52,8 +89,20 @@ const HomeScreen = () => {
 
             </ScrollView>
 
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                    {list.map((item,index)=>{
+                        <Pressable>
+                            
+                            <Image style={{width:50,height:50,resizeMode:"contain"}}  source={{uri:item.image}} />
+
+                            <Text style={{textAlign:"center",fontSize:12,fontWeight:"500",marginTop:5}}>{item?.name} </Text>
+
+                        </Pressable>
+                    }) }
+            </ScrollView>
 
 
+                <Text>fajjsdfaf</Text>
         </SafeAreaView>
     )
 }
