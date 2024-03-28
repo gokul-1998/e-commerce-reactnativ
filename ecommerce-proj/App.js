d89d7bed5e41118e8d5189f7fb5b7b1ea72a6b3a
Gokul <82328858+gokul-1998@users.noreply.github.com>
2024-03-28 08:09:13 +0530

    added till 2.56

--- ecommerce-proj/App.js
+++ ecommerce-proj/App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StackNavigator from './navigation/StackNavigator';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+
+import axios from 'axios';
 
 // to use axios base url
-import axios from 'axios';
+
 axios.defaults.baseURL = 'http://192.168.21.57:8000';
 
 export default function App() {
   return (
+    <>
+    <Provider store={store}>
+
     <StackNavigator />
+    </Provider>
+    </>
   );
 }
 
